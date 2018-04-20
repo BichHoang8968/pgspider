214422ba1b9186948436bc0a410ca25b1625df78
g3033310 <g3033310@e780d44d-2ab3-41a2-bc56-fde8555b7da3>
2018-04-20 04:21:31 +0000

    [fix] spd_fdw--1.0
    git-svn-id: https://tccloud2.toshiba.co.jp/accio/svn/accio/trunk/postgres_remodeling/sources/DDSF/postgresql-10.1@490 e780d44d-2ab3-41a2-bc56-fde8555b7da3

--- contrib/spd_fdw/spd_fdw--1.0.sql
+++ contrib/spd_fdw/spd_fdw--1.0.sql
@@ -1,20 +1,18 @@
 /* contrib/spd_fdw/spd_fdw--1.0.sql */
 
--- complain if script
-	is sourced in psql, rather than via CREATE EXTENSION
-		\ echo Use "CREATE EXTENSION spd_fdw" to load this file.\ quit
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION spd_fdw" to load this file. \quit
 
-		CREATE FUNCTION spd_fdw_handler()
-		RETURNS fdw_handler
-		AS 'MODULE_PATHNAME'
-		LANGUAGE C STRICT;
+CREATE FUNCTION spd_fdw_handler()
+RETURNS fdw_handler
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
 
-CREATE		FUNCTION
-spd_fdw_validator(text[], oid)
+CREATE FUNCTION spd_fdw_validator(text[], oid)
 RETURNS void
-			AS 'MODULE_PATHNAME'
-			LANGUAGE C STRICT;
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
 
-CREATE		FOREIGN DATA WRAPPER spd_fdw
-			HANDLER spd_fdw_handler
-			VALIDATOR spd_fdw_validator;
+CREATE FOREIGN DATA WRAPPER spd_fdw
+  HANDLER spd_fdw_handler
+  VALIDATOR spd_fdw_validator;
