a8b77b0e1f282891f79d892816086eb568670e71
minhla <minh1.leanh@toshiba.co.jp>
2021-03-01 08:31:18 +0000

    [fix-review] reformat comments in line 4549

--- contrib/pgspider_core_fdw/pgspider_core_fdw.c
+++ contrib/pgspider_core_fdw/pgspider_core_fdw.c
@@ -4545,8 +4545,8 @@ spd_GetForeignPaths(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid)
 
 			/*
 			 * The ECs need to reached canonical state. Otherwise, pathkeys of
-			 * parquet_s3_fdw could be rendered non-canonical
-			*/
+			 * parquet_s3_fdw could be rendered non-canonical.
+			 */
 			if (strcmp(fdw->fdwname, PARQUET_S3_FDW) == 0)
 				pChildInfo->root->ec_merging_done = root->ec_merging_done;
 
