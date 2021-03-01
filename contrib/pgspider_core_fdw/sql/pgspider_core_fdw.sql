aca54af49e91e05ad87ed6b70e754315add2f758
Le Anh Minh <minhla@toshiba-tsdv.com>
2021-03-01 17:18:39 +0700

    [Fix-review] Add testcaseID

--- contrib/pgspider_core_fdw/sql/pgspider_core_fdw.sql
+++ contrib/pgspider_core_fdw/sql/pgspider_core_fdw.sql
@@ -706,8 +706,13 @@ DROP SERVER pgspider_svr CASCADE;
 DROP EXTENSION pgspider_core_fdw CASCADE;
 
 --Clean
+--Testcase 270:
 DROP EXTENSION postgres_fdw CASCADE;
+--Testcase 271:
 DROP EXTENSION file_fdw CASCADE;
+--Testcase 272:
 DROP EXTENSION sqlite_fdw CASCADE;
+--Testcase 273:
 DROP EXTENSION tinybrace_fdw CASCADE;
+--Testcase 274:
 DROP EXTENSION mysql_fdw CASCADE;
