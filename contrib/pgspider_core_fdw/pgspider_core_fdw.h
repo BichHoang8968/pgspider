70c74845f978a5103646f5790192427d6f7a7b45
t-kataym <taiga.katayama@toshiba.co.jp>
2023-01-12 17:14:17 +0900

    support the insert routing feature for a simple insert

--- contrib/pgspider_core_fdw/pgspider_core_fdw.h
+++ contrib/pgspider_core_fdw/pgspider_core_fdw.h
@@ -13,6 +13,7 @@
 #ifndef PGSPIDER_CORE_FDW_H
 #define PGSPIDER_CORE_FDW_H
 
+#include "foreign/fdwapi.h"
 #include "foreign/foreign.h"
 #include "lib/stringinfo.h"
 #include "nodes/pathnodes.h"
@@ -23,6 +24,57 @@
 /* For checking single node or multiple node */
 #define SPD_SINGLE_NODE	1
 #define IS_SPD_MULTI_NODES(nodenum) (nodenum > SPD_SINGLE_NODE)
+
+enum SpdServerstatus
+{
+	ServerStatusAlive,
+	ServerStatusIn,
+	ServerStatusDead,
+};
+
+/* This structure stores child pushdown information about child path. */
+typedef struct ChildPushdownInfo
+{
+	RelOptKind	relkind;			/* Child relation kind. */
+	Path		*path;				/* The best child path for child GetForeignPlan. */
+
+	bool		orderby_pushdown;	/* True if child node can pushdown ORDER BY to remote server. */
+	bool		limit_pushdown;		/* True if child node can pushdown LIMIT/OFFSET to remote server. */
+}			ChildPushdownInfo;
+
+/* This structure stores child information about plan. */
+typedef struct ChildInfo
+{
+	/* USE ONLY IN PLANNING */
+	RelOptInfo *baserel;
+	RelOptInfo *input_rel_local;	/* Child input relation for creating child upper paths. */
+	List	   *url_list;
+	AggPath    *aggpath;
+#ifdef ENABLE_PARALLEL_S3
+	Value	   *s3file;
+#endif
+	RelOptInfo *joinrel;		/* Child relation info for join pushdown */
+	FdwRoutine *fdwroutine;
+
+	ChildPushdownInfo pushdown_info;	/* Child pushdown information */
+
+	/* USE IN BOTH PLANNING AND EXECUTION */
+	PlannerInfo *root;
+	Plan	   *plan;
+	enum SpdServerstatus child_node_status;
+	Oid			server_oid;		/* child table's server oid */
+	Oid			oid;			/* child table's table oid */
+	Agg		   *pAgg;			/* "Aggref" for Disable of aggregation push
+								 * down servers */
+	bool		pseudo_agg;		/* True if aggregate function is calcuated on
+								 * pgspider_core. It mean that it is not
+								 * pushed down. This is a cache for searching
+								 * pPseudoAggList by server oid. */
+	List	   *fdw_private;	/* Private information of child fdw */
+
+	/* USE ONLY IN EXECUTION */
+	int			index_threadinfo;	/* index for ForeignScanThreadInfo array */
+}			ChildInfo;
 
  /* in pgspider_core_deparse.c */
 extern bool spd_is_foreign_expr(PlannerInfo *, RelOptInfo *, Expr *);
@@ -50,5 +102,13 @@ extern int	spdExtractConnectionOptions(List *defelems,
 
 /* in pgspider_core_option.c */
 extern int	spd_get_node_num(RelOptInfo *baserel);
+
+/* in pgspider_core_fdw.c */
+Oid spd_serverid_of_relation(Oid foreigntableid);
+void spd_calculate_datasouce_count(Oid foreigntableid, int *nums, Oid **oid);
+void spd_servername_from_tableoid(Oid foreigntableid, char *srvname);
+void spd_ip_from_server_name(char *serverName, char *ip);
+List *spd_ParseUrl(List *spd_url_list);
+List *spd_create_child_url(List *spd_url_list, ChildInfo *pChildInfo, int node_num);
 
 #endif							/* PGSPIDER_CORE_FDW_H */
---
+++ contrib/pgspider_core_fdw/pgspider_core_routing.c
@@ -0,0 +1,508 @@
+/*-------------------------------------------------------------------------
+ *
+ * pgspider_core_routing.c
+ *		  Management of insert target node
+ *
+ * Portions Copyright (c) 2022, TOSHIBA CORPORATION
+ *
+ * IDENTIFICATION
+ *		  contrib/pgspider_core_fdw/pgspider_core_routing.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef OMIT_INSERT_ROUNDROBIN
+
+#include <stddef.h>
+#include "postgres.h"
+#include "access/table.h"
+#include "c.h"
+#include "foreign/fdwapi.h"
+#include "fmgr.h"
+#include "lib/dshash.h"
+#include "nodes/parsenodes.h"
+#include "nodes/pg_list.h"
+#include "pg_config_manual.h"
+#include "pgspider_core_fdw.h"
+#include "postgres_ext.h"
+#include "utils/datum.h"
+#include "utils/dsa.h"
+#include "utils/elog.h"
+#include "utils/lsyscache.h"
+#include "utils/memutils.h"
+#include "utils/rel.h"
+#include "utils/relcache.h"
+#ifndef WITHOUT_KEEPALIVE
+#include "pgspider_keepalive/pgspider_keepalive.h"
+#endif
+#include "pgspider_core_routing.h"
+
+/* Structure for data stored in dsa. */
+typedef struct SpdInststShared {
+	dshash_table_handle	hash_handle;
+	int					tranche_id;
+} SpdInststShared;
+
+/* Structure for data stored in global variable. */
+typedef struct SpdInststGlb {
+	SpdInststShared	   *shared;
+	dsa_area		   *area;
+	dshash_table	   *hash;
+} SpdInststGlb;
+
+typedef struct SpdInstgtElem
+{
+	Oid		parent;		/* Parent table oid: hash key (must be first) */
+	Oid		child;		/* Child table oid */
+	char	tablename[NAMEDATALEN];	/* Child table name */
+}			SpdInstgtElem;
+
+extern bool				throwCandidateError;
+static SpdInststGlb		spd_instst_glb;
+
+#define g_spd_instst_shared		(spd_instst_glb.shared)
+#define g_spd_instst_area		(spd_instst_glb.area)
+#define g_spd_instst_hash		(spd_instst_glb.hash)
+
+/**
+ * spd_inscand_updatable
+ * 		
+ */
+static void
+spd_inscand_updatable(ChildInfo *pChildInfo, int node_num)
+{
+	int		i;
+
+	for (i = 0; i < node_num; i++)
+	{
+		Relation	rel = NULL;
+		int			updatable = 0;
+		ChildInfo  *pChild = &pChildInfo[i];
+
+		PG_TRY();
+		{
+			Oid			server_oid = spd_serverid_of_relation(pChild->oid);
+			FdwRoutine *fdwroutine = GetFdwRoutineByServerId(server_oid);
+
+			rel = table_open(pChild->oid, NoLock); // ToDo close
+
+			updatable = fdwroutine->IsForeignRelUpdatable(rel);
+		}
+		PG_CATCH();
+		{
+			int		elevel;
+
+			pChild->child_node_status = ServerStatusDead;
+			FlushErrorState();
+			if (throwCandidateError)
+				elevel = ERROR;
+			else
+				elevel = WARNING;
+			ereport(elevel, (errmsg("GetForeignPlan of child[%d] failed.", i))); // ToDo :message
+		}
+		PG_END_TRY();
+
+		if (rel)
+			table_close(rel, NoLock);
+
+		if ((updatable & (1 << CMD_INSERT)) == 0)
+			pChild->child_node_status = ServerStatusDead; // ToDo: un-updatable state
+	}
+
+	/* ToDo: Check the number of candidates. */
+
+}
+
+/**
+ * 
+ */
+static void
+spd_inscand_alive(ChildInfo *pChildInfo, int node_num)
+{
+	int		i;
+
+	for (i = 0; i < node_num; i++)
+	{
+		char		ip[NAMEDATALEN] = {0};
+		Oid			server_oid;
+		ForeignServer	*fs;
+		ForeignDataWrapper *fdw;
+
+		server_oid = spd_serverid_of_relation(pChildInfo[i].oid);
+		fs = GetForeignServer(server_oid);
+		fdw = GetForeignDataWrapper(fs->fdwid);
+		
+		spd_ip_from_server_name(fs->servername, ip);
+#ifndef WITHOUT_KEEPALIVE
+		if (check_server_ipname(fs->servername, ip))
+		{
+#endif
+
+#ifndef WITHOUT_KEEPALIVE
+		}
+		else
+		{
+			int		elevel;
+
+			pChildInfo->child_node_status = ServerStatusDead;
+			if (throwCandidateError)
+				elevel = ERROR;
+			else
+				elevel = WARNING;
+			ereport(elevel, (errmsg("."))); // ToDo
+		}
+#endif
+	}
+	
+	/* Check the number of candidates. */
+}
+
+/**
+ * spd_inscand_get
+ * 		Get a candidates of insert target on prepare phase.
+ */
+void
+spd_inscand_get(ChildInfo *pChildInfo, int node_num)
+{
+	int		i;
+	int		num_targets = 0;
+
+	/* Check the number of candidates. */
+	for (i = 0; i < node_num; i++)
+	{
+		if (pChildInfo[i].child_node_status == ServerStatusAlive)
+			num_targets++;
+	}
+	if (num_targets == 0)
+		ereport(ERROR, (errmsg("There is no candidate for insert.")));
+
+	spd_inscand_updatable(pChildInfo, node_num);
+
+	spd_inscand_alive(pChildInfo, node_num);
+}
+
+/**
+ * spd_get_spdurl_in_slot
+ * 		Get SPDURL column value as string in slot.
+ */
+static char *
+spd_get_spdurl_in_slot(TupleTableSlot *slot, TupleDesc	tupdesc)
+{
+	int			attnum;		/* Attrnum of SPDURL column */
+	Form_pg_attribute attr;
+	Datum		value;
+	bool		isnull;
+	Oid			typefnoid;
+	bool		isvarlena;
+	FmgrInfo	flinfo;
+	char	  *spdurl;
+	
+	/* Get SPDURL column value as Datum. */
+	attnum = tupdesc->natts;
+	attr = TupleDescAttr(tupdesc, attnum - 1);
+	value = slot_getattr(slot, attnum, &isnull);
+
+	/* Do nothing if SPDURL is NULL. */
+	if (isnull)
+		return NULL;
+
+	/* Get SPDURL column value as string. */
+	getTypeOutputInfo(attr->atttypid, &typefnoid, &isvarlena);
+	fmgr_info(typefnoid, &flinfo);
+	spdurl = OutputFunctionCall(&flinfo, value);
+
+	return spdurl;
+}
+
+/**
+ * spd_get_server_from_slot
+ * 		Get a server name of SPDURL in slot.
+ */
+static const char *
+spd_get_server_from_slot(TupleTableSlot *slot, TupleDesc tupdesc)
+{
+	char   *spdurl;
+	List		 *url_list;
+	List		 *url_parse_list;
+	const char   *spdurl_server;
+
+	/* Get SPDURL value. */
+	spdurl = spd_get_spdurl_in_slot(slot, tupdesc);
+
+	/* Do nothing if SPDURL is not specified. */
+	if (spdurl == NULL)
+		return NULL;
+
+	/* Get a server name in SPDURL. */
+	url_list = spd_ParseUrl(list_make1(spdurl));
+	url_parse_list = (List *) list_nth(url_list, 0);
+	spdurl_server = (char *) list_nth(url_parse_list, 0);
+
+	return spdurl_server;
+}
+
+/**
+ * spd_inscand_spdurl
+ * 		Remove un-related tables from candidates based on SPDURL
+ * 		column value.
+ * 
+ * 		child_node_status will be set to {DoTo} from ServerStatusAlive if un-related table.
+ */
+void
+spd_inscand_spdurl(TupleTableSlot *slot, Relation rel, ChildInfo *pChildInfo, int node_num)
+{
+	TupleDesc			tupdesc;
+	const char	  *spdurl_server;
+	int					i;
+
+	/* Get a server name. */
+	tupdesc = RelationGetDescr(rel);
+	spdurl_server = spd_get_server_from_slot(slot, tupdesc);
+
+	/* Do nothing if server name is not specified. */
+	if (spdurl_server == NULL)
+		return;
+
+	for (i = 0; i < node_num; i++)
+	{
+		char			srvname[NAMEDATALEN];
+		ChildInfo	  *pChild = &pChildInfo[i];
+		
+		if (pChild->child_node_status != ServerStatusAlive)
+			continue;
+
+		spd_servername_from_tableoid(pChild->oid, srvname);
+		if (strcmp(spdurl_server, srvname) != 0)
+			pChild->child_node_status = ServerStatusDead; /* ToDo: not spdurl target state */
+	}
+
+	/* Check the number of candidates. */
+
+}
+
+/**
+ * spd_instgt_last_table
+ * 		Find the last insert target. If it is found, the 2nd argument will
+ * 		be set to true. Even if the table has been renamed, the 2nd argument
+ * 		will be set to false.
+ * 		This function returns a hasn entry for the target.
+ * 		Write lock for shared hash will be acquired.
+ */
+static SpdInstgtElem *
+spd_instgt_last_table(Oid parent, bool *found)
+{
+	SpdInstgtElem  *entry;
+	char		   *relname;
+
+	entry = dshash_find_or_insert(g_spd_instst_hash, &parent, found);
+	
+	if (!(*found))
+		return entry;
+
+	/* Get the current table name of child table. */
+	relname = get_rel_name(entry->child);
+
+	/* Check whether child table was renamed. */
+	if (strcmp(relname, entry->tablename) != 0)
+	{
+		*found = false;
+		return entry;
+	}
+	
+	return entry;
+}
+
+/**
+ * spd_instgt_choose
+ * 		Choose one child table from candidate for insert.
+ */
+static int
+spd_instgt_choose(char *prev_name, ChildInfo *pChildInfo, int node_num)
+{
+	int				i;
+
+	/*
+	 * If the previous inserted table is memorized, search the position
+	 * in candidates and choose the next one. 
+	 */
+	if (prev_name != NULL)
+	{
+		for (i = 0; i < node_num; i++)
+		{
+			char *relname = get_rel_name(pChildInfo[i].oid);
+			int cmp = strcmp(prev_name, relname);
+
+			if (cmp >= 0)
+				continue;
+
+			if (pChildInfo[i].child_node_status == ServerStatusAlive)
+				return i;
+		}
+
+		/* 
+		 * Here, the previous table name is larget in candidates.
+		 * So the first child table in candidate will be choosen.
+		 */
+	}
+
+	/* Find the first child table in candidates. */
+	for (i = 0; i < node_num; i++)
+	{
+		if (pChildInfo[i].child_node_status == ServerStatusAlive)
+			return i;
+	}
+
+	/* Should not reach here. */
+	ereport(ERROR, (errmsg("Cannot find an insert target.")));
+}
+
+/**
+ * spd_instst_get_target
+ * 		Choose one child table from candidate for insert 
+ * 		and memorize it in shared memory.
+ */
+int
+spd_instst_get_target(Oid parent, ChildInfo *pChildInfo, int node_num)
+{
+	SpdInstgtElem  *entry;
+	bool			found;
+	char		   *prev_name;
+	int				i;
+	char		   *relname;
+
+	/* Find the last target and get a lock for the shared hash. */
+	entry = spd_instgt_last_table(parent, &found);
+
+	/* Choose the insert target. */
+	if (found)
+		prev_name = entry->tablename;
+	else
+		prev_name = NULL;
+
+	i = spd_instgt_choose(prev_name, pChildInfo, node_num);
+
+	/* Update target information in shared memory. */
+	relname = get_rel_name(pChildInfo[i].oid);
+	entry->child = pChildInfo[i].oid;
+	strcpy(entry->tablename, relname);
+
+	/* Release the lock. */
+	dshash_release_lock(g_spd_instst_hash, entry);
+
+	return i;
+}
+
+static dshash_parameters
+spd_instgt_dshash_params(int tranche_id)
+{
+	dshash_parameters params = {
+		sizeof(Oid),
+		sizeof(SpdInstgtElem),
+		dshash_memcmp,
+		dshash_memhash,
+		tranche_id
+	};
+	return params;
+}
+
+/**
+ * spd_instgt_init_dsa
+ * 		Create a dynamic shared memory area and a shared data in it.
+ *		The location for the shared data is stored in the first argument.
+ *		One of the shared data is a hash table which manages an insert target.
+ */
+static void
+spd_instgt_init_dsa(SpdInstgtLocation *location)
+{
+	dsa_area		   *area;
+	dsa_pointer			dp;
+	SpdInststShared	   *its;
+	int					tranche_id;
+	dshash_parameters	hash_params;
+	MemoryContext		oldMemoryContext;
+
+	/* Use the top memory context to keep global variables during a worker process alive. */
+	oldMemoryContext = MemoryContextSwitchTo(TopMemoryContext);
+
+	/*
+	 * Create a duynamic shared memory area.
+	 * This area is kept even if backend is detached or query is finished.
+	 */
+	area = dsa_create(LWLockNewTrancheId());
+	dsa_pin(area);
+	dsa_pin_mapping(area);
+
+	/* Set the global variable. */
+	g_spd_instst_area = area;
+
+	/* Create the hash table. */
+	tranche_id = LWLockNewTrancheId();
+	hash_params = spd_instgt_dshash_params(tranche_id);
+	g_spd_instst_hash = dshash_create(area, &hash_params, NULL);
+
+	MemoryContextSwitchTo(oldMemoryContext);
+
+	/* Create and set shared variables. */
+	dp = dsa_allocate0(area, sizeof(SpdInststShared));
+	its = (SpdInststShared *) dsa_get_address(area, dp);
+	its->hash_handle = dshash_get_hash_table_handle(g_spd_instst_hash);
+	its->tranche_id = tranche_id;
+
+	/* Register the location of shared variables in shared memory. */
+	location->handle = dsa_get_handle(area);
+	location->pointer = dp;
+}
+
+/**
+ * spd_instgt_init_shm
+ *		Initialize a shared memory for insert target. The shared memory stores
+ *		a location for a dynamic shared memory for insert target.
+ * 
+ *		Node:
+ *			RequestAddinShmemSpace(sizeof(SpdInstgtLocation)); should be
+ *			called in CreateSharedMemoryAndSemaphores() during server
+ *			initialization.
+ */
+void
+spd_instgt_init_shm(void)
+{
+	SpdInstgtLocation	*location;
+	bool		found;
+
+	/* Get a lock for use of shared memory. */
+	LWLockAcquire(AddinShmemInitLock, LW_EXCLUSIVE);
+
+	location = (SpdInstgtLocation *) ShmemInitStruct("location of insert target",
+													 sizeof(SpdInstgtLocation),
+													 &found);
+
+	if (!found)
+	{
+		/* Initialize the variable in dynamic shared memory. */
+		spd_instgt_init_dsa(location);
+
+		/* Set the global variable. */
+		g_spd_instst_shared = (SpdInststShared *) dsa_get_address(g_spd_instst_area, location->pointer);
+	}
+	else
+	{
+		dshash_parameters	hash_params;
+		MemoryContext		oldMemoryContext;
+
+		oldMemoryContext = MemoryContextSwitchTo(TopMemoryContext);
+
+		/* Set global variables. */
+		g_spd_instst_area = dsa_attach(location->handle);
+		dsa_pin_mapping(g_spd_instst_area);
+
+		g_spd_instst_shared = (SpdInststShared *) dsa_get_address(g_spd_instst_area, location->pointer);
+		hash_params = spd_instgt_dshash_params(g_spd_instst_shared->tranche_id);
+
+		g_spd_instst_hash = dshash_attach(g_spd_instst_area, &hash_params, g_spd_instst_shared->hash_handle, NULL);
+
+		MemoryContextSwitchTo(oldMemoryContext);
+	}
+
+	LWLockRelease(AddinShmemInitLock);
+}
+
+#endif  /* OMIT_INSERT_ROUNDROBIN */
